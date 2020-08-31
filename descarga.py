1b8b565f2827021c93b547add8b989e6b71552cd
Johan Mendez <jsmendezh@unal.edu.co>
2020-08-31 14:33:24 -0500

    construcción de la clase

--- descarga.py
+++ descarga.py
@@ -8,9 +8,13 @@ import sys
 from datetime import datetime
 import numpy as np
 import os
-
+import drms
 
 class download_files(object):
+    
     def __init__(self, start, end, wave, cadence, series="aia.lev1_euv_!2s",
                  segmet="image", email=None, odir=None, overwrite=True,
                  max_con=1, dfmt="%Y/%m/%d %H:%M:%S"):
+
+        self.awavs = [94, 131, 171, 193, 211, 304, 335, 1600, 1700]
+        self.asegs = ["image", "spike", "None", "magnetogram"]
